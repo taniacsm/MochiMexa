a15a5e0c2d3c421289b3494a018a653bba75ba43
Frida Pilar Luna <frida.luna.gb@gmail.com>
2026-08-05 19:29:33 -0600

    footer

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -13,19 +13,19 @@ document.addEventListener("DOMContentLoaded", () => {
             console.error("Error al importar el NavBar:", error)
         })
 
-    // fetch("footer.html")
-    //     .then(response => {
-    //         if (!response.ok) {
-    //             throw new Error("Error al cargar el Footer")
-    //         }
-    //         return response.text()
-    //     })
-    //     .then(data => {
-    //         document.getElementById("mochiFooter").innerHTML = data;
-    //     })
-    //     .catch(error => {
-    //         console.error("Error al importar el NavBar:", error)
-    //     })
+     fetch("footer.html")
+         .then(response => {
+             if (!response.ok) {
+                 throw new Error("Error al cargar el Footer")
+             }
+             return response.text()
+         })
+         .then(data => {
+             document.getElementById("mochiFooter").innerHTML = data;
+         })
+         .catch(error => {
+             console.error("Error al importar el NavBar:", error)
+         })
 
     //Despliegue de carrito
     fetch('../pages/despliegueCarrito.html') // 
