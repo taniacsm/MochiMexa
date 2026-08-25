ba260c8a9e571b99526e73f6b02285bdf2b2ffcb
Maribel Serrano <maribelserrano611@gmail.com>
2026-08-24 19:41:27 -0600

    Actualizar Verificaciones.sql
    Se agrega la verificación de la nueva tabla contrasenia.

---
+++ Verificaciones.sql
@@ -0,0 +1,21 @@
+-- =========================================================
+-- VERIFICACION
+-- =========================================================
+USE mochimexa;
+
+SELECT * FROM rol;
+SELECT * FROM usuario;
+SELECT * FROM contrasenia;
+SELECT * FROM categoria;
+SELECT * FROM producto;
+SELECT * FROM direccion;
+SELECT * FROM carrito;
+SELECT * FROM carrito_detalle;
+SELECT * FROM pedido;
+SELECT * FROM pedido_direccion;
+SELECT * FROM pedido_detalle;
+SELECT * FROM metodo_pago;
+SELECT * FROM envio;
+SELECT * FROM resenias;
+SELECT * FROM promociones;
+SELECT * FROM producto_promocion;
